528251c5d93037e1ef4270c6da93da7860ebda16
tylsonb <bellotj@gmail.com>
2024-07-09 18:32:00 -0400

    Implement dotenv for managing database connection credentials

--- src/model/product.model.js
+++ src/model/product.model.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const { Sequelize, DataTypes, Model, UUIDV4} = require('sequelize');
 
-const sequelize = new Sequelize ("bellotj_product", "bellotj_root", "rXAje#7#M8N9.4F",{
-    host: 'mysql-bellotj.alwaysdata.net',
-    dialect: 'mysql',
-    port: 3306
-})
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+    }
+);
 
 class Product extends Model {}
 
@@ -34,7 +40,7 @@ is_stock:{
 
 module.exports = Product;
 
-/* async function testConnection() {
+async function testConnection() {
     try {
         await sequelize.authenticate();
         console.log("Conexion Exitosa")
@@ -43,4 +49,4 @@ module.exports = Product;
     }
 }
 
-testConnection(); */
+testConnection();
